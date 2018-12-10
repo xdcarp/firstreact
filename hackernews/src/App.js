8a0b010efef7b6afa6f242d4959d0ac96233386b
M Gabriela Gutierrez <dcarp2001@hotmail.com>
2018-12-10 08:55:43 -0300

    Hasta destructirung

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -21,6 +21,13 @@ const list = [
   }
 ];
 
+function isSearched(searchTerm){
+  return function(item) {
+    return !searchTerm || 
+      item.title.toLowerCase().includes(searchTerm.toLowerCase());
+  }
+}
+
 class App extends Component {
 
   constructor(props){
@@ -28,14 +35,35 @@ class App extends Component {
 
     this.state = {
       list: list,
+      searchTerm: '',
     };
+
+    this.onSearchChange = this.onSearchChange.bind(this);
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onSearchChange(event){
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  onDismiss(id){
+    function isNotId(item){
+      return item.objectID !== id;
+    }
+
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list:updatedList });
   }
 
   render() {
-      return (
+    const { searchTerm, list} = this.state;  //destructuring
+    return (
       <div className="App">
+        <form>
+          <input type="text" onChange={this.onSearchChange}/>
+        </form>        
         {
-          this.state.list.map(item =>             
+          list.filter(isSearched(searchTerm)).map(item =>             
               <div key={item.objectID}>
                 <span>
                   <a href={item.url}>{item.title}</a>
@@ -43,6 +71,14 @@ class App extends Component {
                 <span>{item.author}</span>
                 <span>{item.num_comments}</span>
                 <span>{item.points}</span>
+                <span>
+                  <button
+                    onClick={ () => this.onDismiss(item.objectID)}
+                    type="button"
+                  >
+                    Dismiss
+                  </button>
+                </span>
               </div>
           )}
       </div>
